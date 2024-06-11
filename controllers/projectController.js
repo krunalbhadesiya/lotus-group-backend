b36ab871c7b0b7f9b23f32110f99137f5e3be6d2
krunalbhadesiya <2101031000089@silveroakuni.ac.in>
2024-06-11 16:11:16 +0530

    Refactor projectController.js and add new route to get project by ID

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,20 +1,17 @@
-import express from 'express';
 import Project from '../models/Project.js';
 
-const router = express.Router();
-
 // Get all projects
-router.get('/', async (req, res) => {
+export const getAllProjects = async (req, res) => {
   try {
     const projects = await Project.find();
     res.json(projects);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-});
+};
 
-// Get a project by ID
-router.get('/:id', async (req, res) => {
+// Get project by ID
+export const getProjectById = async (req, res) => {
   try {
     const project = await Project.findById(req.params.id);
     if (!project) {
@@ -24,10 +21,10 @@ router.get('/:id', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-});
+};
 
-// Add a new project
-router.post('/', async (req, res) => {
+// Create a new project
+export const createProject = async (req, res) => {
   try {
     const newProject = new Project(req.body);
     await newProject.save();
@@ -35,10 +32,10 @@ router.post('/', async (req, res) => {
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
-});
+};
 
 // Update a project
-router.put('/:id', async (req, res) => {
+export const updateProject = async (req, res) => {
   try {
     const project = await Project.findByIdAndUpdate(req.params.id, req.body, { new: true });
     if (!project) {
@@ -48,10 +45,10 @@ router.put('/:id', async (req, res) => {
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
-});
+};
 
 // Delete a project
-router.delete('/:id', async (req, res) => {
+export const deleteProject = async (req, res) => {
   try {
     const project = await Project.findByIdAndDelete(req.params.id);
     if (!project) {
@@ -61,6 +58,4 @@ router.delete('/:id', async (req, res) => {
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
-});
-
-export default router;
+};
--- routes/projects.js
+++ routes/projects.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getAllProjects, createProject, updateProject, deleteProject } from '../controllers/projectController.js';
+import { getAllProjects, getProjectById, createProject, updateProject, deleteProject } from '../controllers/projectController.js';
 
 const router = express.Router();
 
 router.get('/', getAllProjects);
+router.get('/:id', getProjectById); // New route to get a project by ID
 router.post('/', createProject);
 router.put('/:id', updateProject);
 router.delete('/:id', deleteProject);
