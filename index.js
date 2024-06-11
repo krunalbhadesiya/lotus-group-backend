867db21b7dcd51b1bc8a4794068befa9ffb34776
krunalbhadesiya <2101031000089@silveroakuni.ac.in>
2024-06-11 13:05:39 +0530

    Update dependencies and add CORS middleware

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
 
 // Import routes
-import home from './routes/home.js';
-import contactsRouter from './routes/contacts.js';
-import projectsRouter from './routes/projects.js';
+import home from "./routes/home.js";
+import contactsRouter from "./routes/contacts.js";
+import projectsRouter from "./routes/projects.js";
 
 // Load environment variables
 dotenv.config();
 
 // Middlewares
 const app = express();
-app.use(express.json({ limit: '10mb' }));  // Increased limit for large JSON payloads
+app.use(express.json({ limit: "10mb" })); // Increased limit for large JSON payloads
+
+// CORS middleware
+app.use(
+  cors({
+    origin: "https://lotusgroup.vercel.app/",
+    methods: ["POST", "GET", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 
 // Routes
 app.use("/home", home);
@@ -21,9 +31,10 @@ app.use("/api/projects", projectsRouter);
 
 // MongoDB Connection
 const mongoURI = process.env.MONGODB_URI;
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Server connection
 const port = process.env.PORT || 9001;
--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Welcome, to Lotus Group🙏🙏🙏');
 });
 
 export default router;
